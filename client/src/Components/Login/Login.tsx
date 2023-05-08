a986ea5a3899007a781d846cee2c7b27d248f899
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2023-05-08 18:56:37 +0200

    login form created and Open Sans font added

--- client/src/Components/Login/Login.tsx
+++ client/src/Components/Login/Login.tsx
@@ -4,9 +4,21 @@ import styles from "./Login.module.sass"
 
 export const Login: FunctionComponent = () => {
 
-  return (
-	  <>
-		  Login
-	  </>
-  );
+	const handleSubmit = (e:any) =>{
+		e.preventDefault()
+	}
+
+	return (
+		<>
+			<div className={styles.LoginPage}>
+				<form className={styles.LoginPage_Form} onSubmit={handleSubmit}>
+					<h1 className={styles.LoginPage_Form_header}>Sign in</h1>
+					<input type="text" className={styles.LoginPage_Form_username} placeholder="provide username..."/>
+					<input type="password" className={styles.LoginPage_Form_password} placeholder="provide password..."/>
+					<button className={styles.LoginPage_Form_showPasswordBtn}>👁</button>
+					<button type="submit" className={styles.LoginPage_Form_submitBtn}>Sign in</button>
+				</form>
+			</div>
+		</>
+	);
 };
