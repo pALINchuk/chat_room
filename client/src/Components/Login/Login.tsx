bf77ff61956c21efb15526c6a1c0eca7e575449f
Maksym Chernetskyi <konomerubokkusu@gmail.com>
2023-05-12 00:30:24 +0200

    login page initial design (sass) completed

--- client/src/Components/Login/Login.tsx
+++ client/src/Components/Login/Login.tsx
@@ -1,11 +1,25 @@
-import React, { FunctionComponent } from 'react';
+import React, {FunctionComponent, useRef} from 'react';
 import styles from "./Login.module.sass"
 
 
 export const Login: FunctionComponent = () => {
 
+	const passwordInputRef : React.RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null)
+	const showPasswordButton: React.RefObject<HTMLButtonElement> = useRef<HTMLButtonElement>(null)
 	const handleSubmit = (e:any) =>{
 		e.preventDefault()
+	}
+
+	const handleShowPasswordClick = (): void =>{
+		if (!passwordInputRef.current) return;
+		if(passwordInputRef.current.type == "password"){
+			passwordInputRef.current.type = "text"
+		}else{
+			passwordInputRef.current.type = "password"
+		}
+
+		if(!showPasswordButton.current) return;
+		showPasswordButton.current.classList.toggle(styles.LoginPage_Form_showPasswordBtn__focused)
 	}
 
 	return (
@@ -13,9 +27,14 @@ export const Login: FunctionComponent = () => {
 			<div className={styles.LoginPage}>
 				<form className={styles.LoginPage_Form} onSubmit={handleSubmit}>
 					<h1 className={styles.LoginPage_Form_header}>Sign in</h1>
-					<input type="text" className={styles.LoginPage_Form_username} placeholder="provide username..."/>
-					<input type="password" className={styles.LoginPage_Form_password} placeholder="provide password..."/>
-					<button className={styles.LoginPage_Form_showPasswordBtn}>👁</button>
+
+					<input type="text" id="usernameInput" className={styles.LoginPage_Form_username} placeholder=" "/>
+					<label htmlFor="usernameInput" className={[styles.LoginPage_Form_usernamePlaceholder, styles.LoginPage_Form_inputPlaceholder].join(' ')}>username</label>
+
+					<input type="password" id="passwordInput" className={styles.LoginPage_Form_password} placeholder=" " ref={passwordInputRef}/>
+					<label htmlFor="passwordInput" className={`${styles.LoginPage_Form_passwordPlaceholder} ${styles.LoginPage_Form_inputPlaceholder}`}>password</label>
+
+					<button className={styles.LoginPage_Form_showPasswordBtn} onClick={handleShowPasswordClick} ref={showPasswordButton}>👁</button>
 					<button type="submit" className={styles.LoginPage_Form_submitBtn}>Sign in</button>
 				</form>
 			</div>
